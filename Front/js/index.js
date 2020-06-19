a52ffbd46d16b1db0e86cabed9c2130e45d12d6c
Alexander <sanekgalchyn@ukr.net>
2020-06-19 19:29:40 +0300

    services

--- Front/js/index.js
+++ Front/js/index.js
@@ -11,3 +11,26 @@ $(window).scroll(function() {
 	}
 	scrollPrev = scrolled;
 });
+
+$(function(){
+	$(".car-img").click(function(event) {
+	  var i_path = $(this).attr('src');
+	  console.log(i_path);
+	  console.log(this);
+	  $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+	  $('#magnify').css({
+		  left: ($(document).width() - $('#magnify').outerWidth())/2,
+		  // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+			  top: ($(window).height() - $('#magnify').outerHeight())/2
+		});
+	  $('#overlay, #magnify').fadeIn('fast');
+	});
+	
+	$('body').on('click', '#close-popup, #overlay', function(event) {
+	  event.preventDefault();
+   
+	  $('#overlay, #magnify').fadeOut('fast', function() {
+		$('#close-popup, #magnify, #overlay').remove();
+	  });
+	});
+  });
