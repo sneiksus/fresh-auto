ccfcfb122c2b7df98eb3e08eaaa596de491c781a
Alexander <sanekgalchyn@ukr.net>
2020-07-01 16:01:46 +0300

    backend

---
+++ Back/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Back/Models/EFContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back.Models
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+
+        public EFContext()
+        {
+            //Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;UserId=root;Password=1111;database=DevDb;");
+        }
+    }
+}
