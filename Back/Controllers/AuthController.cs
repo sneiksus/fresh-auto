d6f45230d5a8b5b52271f4b4f715256a020adf4c
Alexander <sanekgalchyn@ukr.net>
2020-07-06 15:47:23 +0300

    car

--- Back/Controllers/AuthController.cs
+++ Back/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Back.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,12 +24,36 @@ namespace Back.Controllers
                 Content = System.IO.File.ReadAllText(@"D:\projects\Freshauto\Back\Helpers\adm.txt")
             };
         }
-
-        [Authorize(Roles = "admin")]
-        [Route("getrole")]
-        public IActionResult GetRole()
+        [HttpPost]
+        public IActionResult AddCar(Car car)
         {
-            return Ok("Ваша роль: администратор");
+            using(EFContext con = new EFContext())
+            {
+                con.Cars.Add(car);
+                con.SaveChanges();
+                return Ok();
+            }
         }
+        [HttpGet]
+        public IEnumerable<Car> GetCars(Car car)
+        {
+            using (EFContext con = new EFContext())
+            {
+                return con.Cars.ToList();
+            }
+        }
+        [HttpPost]
+
+        public IActionResult DelCar(int num)
+        {
+            using (EFContext con = new EFContext())
+            {
+                var car = con.Cars.SingleOrDefault(x => x.Id == num);
+                con.Cars.Remove(car);
+                con.SaveChanges();
+                return Ok();
+            }
+        }
+
     }
 }
